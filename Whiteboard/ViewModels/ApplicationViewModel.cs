1b161fcfdbb3542b7e777daaa4b3d2594b46f44e
Hubert <40564309+Qzername@users.noreply.github.com>
2024-12-14 19:53:47 +0100

    Add ability to reopen window after closing it

--- Whiteboard/ViewModels/ApplicationViewModel.cs
+++ Whiteboard/ViewModels/ApplicationViewModel.cs
@@ -1,11 +1,23 @@
 ﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using System.Diagnostics;
+using Whiteboard.Views;
 
 namespace Whiteboard.ViewModels
 {
     public class ApplicationViewModel : ViewModelBase
     {
+        public static void OpenApplication()
+        {
+            var desktop = (IClassicDesktopStyleApplicationLifetime)Application.Current!.ApplicationLifetime!;
+           
+            if (desktop.MainWindow!.IsVisible)
+                return;
+        
+            desktop.MainWindow = new MainWindow();
+            desktop.MainWindow.Show();
+        }
+
         public static void CloseApplication()
         {
             var desktop = (IClassicDesktopStyleApplicationLifetime)Application.Current!.ApplicationLifetime!;
