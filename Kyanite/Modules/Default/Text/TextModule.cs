d46d71a7adac46348efd2cb82406c7f8ec67b6b4
Qzername <40564309+Qzername@users.noreply.github.com>
2026-05-06 01:51:22 +0200

    Add text module

---
+++ Kyanite/Modules/Default/Text/TextModule.cs
@@ -0,0 +1,12 @@
+﻿using ReactiveUI;
+using ReactiveUI.SourceGenerators;
+using System.Dynamic;
+
+namespace Kyanite.Modules.Text;
+
+public partial class TextModule : ReactiveObject
+{
+    [Reactive]
+    [Synchronize("text", "### Here you can write your text.")]
+    public partial string Text { get; set; }
+}
---
+++ Kyanite/Modules/Default/Text/TextModuleView.axaml.cs
@@ -0,0 +1,11 @@
+using ReactiveUI.Avalonia;
+
+namespace Kyanite.Modules.Text;
+
+public partial class TextModuleView : ReactiveUserControl<TextModule>
+{
+    public TextModuleView()
+    {
+        InitializeComponent();
+    }
+}
