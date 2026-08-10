4ada3f5273d2549c12198e207a3d7b77ba31d312
Qzername <40564309+Qzername@users.noreply.github.com>
2026-08-10 18:27:36 +0200

    Clear code again

--- Kyanite/Models/ModuleData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Kyanite.Modules;
-
-public struct ModuleData
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-}
--- KyaniteAPI/Models/Module.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace KyaniteAPI.Modules;
-
-public struct Module
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-}
