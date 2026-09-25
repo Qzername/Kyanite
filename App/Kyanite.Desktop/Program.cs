df3e14da81aaf89b77a80cc73ce0b498a21d3f7a
Hubert <40564309+Qzername@users.noreply.github.com>
2026-09-25 20:23:42 +0200

    Enable only one instance running at the same time

--- App/Kyanite.Desktop/Program.cs
+++ App/Kyanite.Desktop/Program.cs
@@ -2,17 +2,35 @@
 using HotAvalonia;
 using Kyanite.Desktop.NotificationServices;
 using System;
+using System.Threading;
 
 namespace Kyanite.Desktop;
 
 internal sealed class Program
 {
+    static readonly string MutexName = "Kyanite";
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // Try to create/acquire the mutex
+        using var mutex = new Mutex(true, MutexName, out bool isNewInstance);
+
+        if (!isNewInstance)
+        {
+            new WindowsNotificationService().Show(
+                "Kyanite",
+                "Instance of this app is already running"
+            );
+
+            return;
+        }
+
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
